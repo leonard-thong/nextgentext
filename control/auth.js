f1992ad8183e1e0d6b6fb94df3ecb9e14f32479e
Leonard <thongyixuen@gmail.com>
2020-11-13 02:15:59 -0500

    Implement login.

---
+++ control/auth.js
@@ -0,0 +1,45 @@
+const signInForm = document.querySelector("#sign_in_form");
+
+if (signInForm) {
+    signInForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const email = signInForm["email"].value;
+        const password = signInForm["password"].value;
+
+        // var cred = await auth.signInWithEmailAndPassword(email, password);
+        // console.log(cred);
+        auth.signInWithEmailAndPassword(email, password).then((cred) => {
+            console.log(cred);
+            url = window.location.origin + "/authenticated/dashboard.html";
+            window.location.replace(url);
+        });
+    });
+}
+
+const signUpForm = document.querySelector("#sign_up_form");
+
+if (signUpForm) {
+    signUpForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const email = signUpForm["email"].value;
+        const password = signUpForm["password"].value;
+
+        auth.createUserWithEmailAndPassword(email, password).then((cred) => {
+            console.log(cred);
+        });
+    });
+}
+
+const logOutButton = document.querySelector("#logout_button");
+
+if (logOutButton) {
+    logOutButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        auth.signOut().then(() => {
+            url = window.location.origin + "/unauthenticated/login.html";
+            window.location.replace(url);
+        });
+    });
+}
